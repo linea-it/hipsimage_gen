60e3444084d6bc148b1cf0ccba335b914143edf8
Cristiano Singulani <crisingulani@gmail.com>
2023-08-07 19:30:23 -0300

    Implemented initial scripts

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # hipsimage_gen
 Collections of scripts to generate HIPs images for LIneA
+
+time ./create_hips.sh 2> error 1> log & 
--- functions.sh
+++ functions.sh
@@ -2,23 +2,79 @@
 
 . env.sh
 
-ALADIN_CMD="java -Xmx20000m -jar $ALADINPATH/AladinBeta.jar -hipsgen "
+if [ -z $HISP_TITLE ];
+then
+  HISP_TITLE='DR2'
+fi
 
-function create_dir() {
-  BAND=$1
+if [ -z $CREATOR_DID ];
+then
+  CREATOR_DID='CDS/P/DES/DR2'
+fi
 
-  mkdir -p outputs/$BAND
-  cd outputs/$BAND
+if [ -z $PIXELCUT_G ];
+then
+  PIXELCUT_G='-1.23 508.7 log'
+fi
+
+
+if [ -z $PIXELCUT_R ];
+then
+  PIXELCUT_R='-2.357 1039 log'
+fi
+
+if [ -z $PIXELCUT_I ];
+then
+  PIXELCUT_I='-2.763 881.7 log'
+fi
+
+function get_config_per_band() {
+  case $1 in
+    'g') echo $PIXELCUT_G ;;
+    'r') echo $PIXELCUT_R ;;
+    'i') echo $PIXELCUT_I ;;
+  esac
 }
 
-function create_hips_initial() {
+MEM=$(expr `grep MemTotal /proc/meminfo | awk '{print $2}'` / 1024)
+THREADS=$(cat /proc/cpuinfo | grep processor | wc -l)
+ALADIN_CMD="java -Xmx${MEM}m -jar $ALADINPATH/AladinBeta.jar -hipsgen maxthread=$THREADS hips_creator='LIneA' obs_title='$HISP_TITLE' creator_did='$CREATOR_DID'"
+
+function create_dir() {
+  OUTPUT_DIR=$1
+  mkdir -p $OUTPUT_DIR
+}
+
+function create_hips_per_band() {
   IMGS=$1
-  PIXELCUT="'$2 log'"
+  BAND=$2
+  PIXELCUT=$(get_config_per_band $BAND)
+  OUTPUT_DIR=$3
+
+  cd $OUTPUT_DIR
+  mkdir -p $BAND
+
   echo "Create initial hips per band"
-  $ALADIN_CMD maxthread=10 incremental=true in="'$IMGS'" out="'.'" hips_creator='LIneA' obs_title='DES_DR2' pixelcut=$PIXELCUT creator_did='AUTH/P/LIN' INDEX TILES JPEG MOC
+  $ALADIN_CMD incremental=true in="'$IMGS'" out="'./$BAND'" pixelcut="'$PIXELCUT'" INDEX TILES JPEG
 }
 
 function create_hips_colour() {
+  OUTPUT_DIR=$1
+
+  cd $OUTPUT_DIR
+  mkdir RGB
+  touch RGB/Moc.fits
+
+  PIXELCUT_G=$(get_config_per_band 'g')
+  PIXELCUT_R=$(get_config_per_band 'r')
+  PIXELCUT_I=$(get_config_per_band 'i')
+
   echo "Generation of one colour HiPS from 3 greyscale HiPS"
-  $ALADIN_CMD maxthread=10 inRed='./hips_i/AUTH_P_LIN' inGreen='./hips_r/AUTH_P_LIN' inBlue='./hips_g/AUTH_P_LIN' cmRed="'${PIXELCUT_I} log'" cmGreen="'${PIXELCUT_R} log'" cmBlue="'${PIXELCUT_G} log'" creator_did='AUTH/P/LIN' out='./hips_RGB' RGB
+  $ALADIN_CMD inRed="'./i/'" inGreen="'./r/'" inBlue="'./g/'" cmRed="'${PIXELCUT_I}'" cmGreen="'${PIXELCUT_R}'" cmBlue="'${PIXELCUT_G}'" out='./RGB' RGB
+}
+
+function clear_trash() {
+  rm -rf $OUTPUT_DIR/g/
+  rm -rf $OUTPUT_DIR/r/
+  rm -rf $OUTPUT_DIR/i/
 }

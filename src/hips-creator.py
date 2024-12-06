6e8feb36bb9746aad2a368637bd9742218b3d6bb
Cristiano Singulani <crisingulani@gmail.com>
2024-12-06 11:33:47 -0300

    Added hipsimg pipeline

--- src/hips-creator.py
+++ src/hips-creator.py
@@ -1,32 +1,17 @@
 #!/usr/bin/env python3
-from dataclasses import dataclass, replace
-from os import environ, execv
-from os.path import expandvars
+from dataclasses import replace, fields
 from pathlib import Path
-from shlex import split
 from shutil import which
-from sys import argv, executable
+from sys import argv
 from typing import Union
-
+import subprocess
+from schemas import ColorConfig, RGBConfig
 from yaml import safe_load
 
 
+SBATCH_COLOR = 'color.sbatch'
+SBATCH_RGB = 'rgb.sbatch'
 COLORS = ["green", "red", "blue"]
-
-
-@dataclass
-class IMGConfig:
-    input_dir: str = './input_%s'
-    output_dir: str = './output_%s'
-    maxthread: str = '10'
-    creator_did: str = 'CDS/P/LSST/DP0'
-    hips_creator: str = 'LIneA'
-    obs_title: str = 'LSST DP0'
-    incremental: str = 'true'
-    mode: str = 'mean'
-    pixelcut: str = '-1.2 400 asinh'
-    cache: str = './tmp_%s'
-    cacheRemoveOnExit: str = 'false'
 
 
 def parse_cmdline():
@@ -37,52 +22,11 @@ def parse_cmdline():
 
     return conffile
 
-def to_path(text):
-    return Path(expandvars(text)).expanduser()
 
+def load_configuration(config):
+    _config = ColorConfig()
+    return replace(_config, **config)
 
-
-def load_configuration(color, main_config):
-
-    img_config = IMGConfig()
-
-    config = main_config.copy() 
-
-    hipsconf = config.get('hipsgen', {'runs': {color:{}}})
-    print(hipsconf)
-    runs = hipsconf.pop("runs")
-    hipsconf.update(runs[color])
-
-    img_config = replace(img_config, **hipsconf)
-
-    config['hipsgen'] = img_config
-
-    #config.in = config.in % color
-    #config.out = config.out % color
-    #config.cache = config.cache % color
-
-    #config.in = to_path(config.in)
-    #config.out = to_path(config.out)
-    #config.cache = to_path(config.cache)
-
-    #img_config = {
-    #    "in": config.in,
-    #    "out": config.out,
-    #    "cache": config.cache,
-    #    "maxthread": '10'
-    #    "creator_did": str = 'CDS/P/LSST/DP
-    #hips_creator: str = 'LIneA'
-    #obs_title: str = 'LSST DP0'
-    #incremental: str = 'true'
-    #mode: str = 'mean'
-    #pixelcut: str = '-1.2 400 asinh'
-    #cache: str = './tmp_%s'
-    #cacheRemoveOnExit: str = 'false'
-    #}
-
-    print(config)
-
-    return config
 
 def find_prog(basename):
     prog = which(basename)
@@ -92,47 +36,44 @@ def find_prog(basename):
 
     return prog
 
-def setup(config):
-    config.sbatch = find_prog(config.sbatch)
-    find_prog(config.rail_slurm_batch)
-    find_prog(config.rail_slurm_py)
 
-    if not config.inputdir.is_dir():
-        raise RuntimeError('input directory not found: %s' % config.inputdir)
+def sbatch(cmd, cwd):
 
-    if config.lepharedir:
-        environ['LEPHAREDIR'] = config.lepharedir
+    process = subprocess.run(cmd, cwd=cwd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    success_msg = 'Submitted batch job'
+    stdout = process.stdout.decode('utf-8')
 
-    if config.lepharework:
-        environ['LEPHAREWORK'] = config.lepharework
+    if success_msg in stdout:
+        job_id = int(stdout.split(' ')[3])
+    else:
+        stderr = process.stderr.decode('utf-8')
+        print("Error: %s", stderr)
+        exit(-1)
 
-def seconds_to_time(seconds):
-    minutes, seconds = divmod(seconds, 60)
-    hours, minutes = divmod(minutes, 60)
+    return job_id
 
-    return '%d:%02d:%02d' % (hours, minutes, seconds)
 
-def run(config):
-    cmd = [config.sbatch]
+def create_config_file(config, name, cwd):
 
-    if config.sbatch_args:
-        cmd += config.sbatch_args
+    _config = Path(cwd, f"{name}.config")
 
-    if config.time_limit is not None:
-        cmd += ['--time=' + seconds_to_time(config.time_limit)]
+    with open(_config, "w") as _conf:
+        for field in fields(config):
+            if field.name == 'input_dir':
+                fieldname = 'in'
+                value = Path(str(getattr(config, field.name))).absolute()
+            elif field.name == 'output_dir':
+                fieldname = 'out'
+                value = Path(cwd, str(getattr(config, field.name)))
+                value.mkdir(exist_ok=True)
+                value = value.absolute()
+            else:
+                fieldname = field.name
+                value = getattr(config, field.name)
+ 
+            _conf.write(f'{fieldname}="{value}"\n')
 
-    cmd += [config.rail_slurm_batch, config.inputdir, config.outputdir, '-a',
-            config.algorithm]
-
-    if config.param_file:
-        cmd += ['-p', config.param_file]
-
-    if config.calib_file:
-        cmd += ['-c', config.calib_file]
-
-    print(' '.join(str(x) for x in cmd))
-    execv(config.sbatch, cmd)
-    raise RuntimeError('error executing slurm')
+    return _config
 
 
 def main():
@@ -144,38 +85,50 @@ def main():
     with open(conffile) as f:
         config = safe_load(f)
 
-    cwd = config.get("cwd", ".")
+    aladin_cmd = config.get("aladin_cmd", "Aladin.jar")
+    cwd = Path(config.get("cwd", "."))
+    cwd.mkdir(exist_ok=True)
+    max_mem = str(config.get("max_mem", "2"))
 
-    print("CWD: %s" % cwd)
+    hips_config = config.pop('hipsgen')
+    hips_runs = hips_config.pop('runs')
+
+    sbatch_color = find_prog(SBATCH_COLOR)
+    sbatch_rgb = find_prog(SBATCH_RGB)
+
+    colors_output = {}
 
     for color in COLORS:
         try:
-            print("\nCOLOR: %s" % color)
-            x = load_configuration(color, config)
-            #setup(config)
-            #run(config)
+            color_config = hips_runs[color]
+            color_config.update(hips_config)
+            config = load_configuration(color_config)
+            color_config = create_config_file(config, color, cwd)
+            colors_output[f'in{color.capitalize()}'] = Path(cwd, config.output_dir).absolute()
         except RuntimeError as e:
             print('Error: %s' % e)
 
+        cmd = ["sbatch", sbatch_color, max_mem, aladin_cmd, color_config]
 
-    #    print("SUBMIT %s..." % x)
-    #    cmd = ["sbatch", config[x]]
-    #    try:
-    #        job_id = sbatch(cmd, cwd)
-    #        parallel_jobs.append(str(job_id))
-    #    except RuntimeError as e:
-    #        exit(-1)
+        try:
+            job_id = sbatch(cmd, cwd)
+            parallel_jobs.append(str(job_id))
+        except RuntimeError as e:
+            exit(-1)
 
-    #print("SUBMIT Consolidate with dependencies: %s" % str(parallel_jobs))
-    #consolidate_job_id = sbatch(["sbatch", "--dependency=afterok:%s" % ",".join(parallel_jobs), config["rgb"]], cwd)
-    #print("Consolidate JobID: %s" % consolidate_job_id)
+    rgb_config = hips_runs['rgb']
+    rgb_config.update(hips_config)
+    rgb_config.update(colors_output)
+
+    rgb_config_obj = replace(RGBConfig(), **rgb_config)
+
+    rgb_config_file = create_config_file(rgb_config_obj, 'rgb', cwd)
+
+    print("Submit Consolidate with dependencies: %s" % str(parallel_jobs))
+    consolidate_job_id = sbatch(["sbatch", "--dependency=afterok:%s" % ",".join(parallel_jobs), sbatch_rgb, max_mem, aladin_cmd, rgb_config_file, str(rgb_config_obj.output_dir)], cwd)
+    print("Consolidate job ID: %s" % consolidate_job_id)
+
 
 if __name__ == '__main__':
-    config = {
-        "rgb": "/lustre/t0/scratch/users/singulani/hipsimage_gen/hipsimage_color.sbatch",
-        "green": "/lustre/t0/scratch/users/singulani/hipsimage_gen/hipsimage_r.sbatch", 
-        "red": "/lustre/t0/scratch/users/singulani/hipsimage_gen/hipsimage_i.sbatch",
-        "blue": "/lustre/t0/scratch/users/singulani/hipsimage_gen/hipsimage_g.sbatch"
-    }
-
     main()
+
